59594f93876a573657660402cf1dbd5d36055708
Harshita Batra <101546087+Harshita-Batra-1@users.noreply.github.com>
2022-06-13 21:02:25 +0530

    Update main.cpp

--- 20swapNo/main.cpp
+++ 20swapNo/main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main()
 {
 // with using 3rd variable
-
-
-// without using 3rd variable
+    int a,b,t;
+    cin>>a>>b;
+    t=a;
+    a=b;
+    b=t;
+    cout<< a << b <<endl;
+    return 0;
 }
+// without using 3rd variable
+a=a+b;
+b=a-b;
+a=a-b;
+
