5ac8e95fa22cb454461e696bb969184c604737cd
Harshita Batra <101546087+Harshita-Batra-1@users.noreply.github.com>
2022-06-15 03:57:29 +0530

    use bool flag=true/=1

--- PrimeCheck.cpp
+++ PrimeCheck.cpp
@@ -31,3 +31,11 @@ int main()
     for(i=a;i<=b;i++){
         if(isPrime(i)){   //calling fuct to check prime
             cout<<i<<endl;}
+bool isPrime(int n){
+    for(int i=2;i<n;i++){   //or i<sqrt(n)
+        if (n%i==0){
+            return false;}
+    }
+    return true;
+}
+    
